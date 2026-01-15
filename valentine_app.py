be2345e2fee591f7ee160872c004f20f29d757f2
suprabdullah96-collab <suprabdullah96@gmail.com>
2026-01-15 17:12:19 +0500

    Update valentine_app.py

--- valentine_app.py
+++ valentine_app.py
@@ -10,16 +10,8 @@ st.markdown("""
         width: 100%;
         border-radius: 20px;
     }
-    .yes-button button {
-        background-color: #4caf50 !important;
-        color: white !important;
-    }
-    .no-button button {
-        background-color: #f44336 !important;
-        color: white !important;
-    }
     </style>
-    """, unsafe_allow_html=True) # Fixed the typo here too!
+    """, unsafe_allow_html=True)
 
 if 'stage' not in st.session_state:
     st.session_state.stage = 0
@@ -55,13 +47,15 @@ if st.session_state.stage < 100:
                 st.session_state.stage += 1
                 st.rerun()
     
-    # If she clicked "No" until the end, only show the Yes button (No button is hidden)
+    # If she clicked "No" until the end, only show the Yes button
     else:
-        if st.button("YES! (No button is gone now 😉)", key="final_yes_btn"):
+        if st.button("YES! (No button is gone now )", key="final_yes_btn"):
             st.session_state.stage = 100
             st.rerun()
 
+# This is what she sees when she says YES
 else:
     st.balloons()
     st.title("Knew you would say yes! ❤️")
+    st.subheader("We'll ride horses together! ")
     st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHp1bmNid2Z4dzRieXp0eXpueXpueXpueXpueXpueXpueXpueXpueCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/K976VvCc8z4xG/giphy.gif")
